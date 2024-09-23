6a7afef3cf95bb60789725cb4ff86e29dc349854
Promise Nmor <nmorchibogu98@gmail.com>
2024-09-23 20:16:13 +0100

    made changes to settings.py file

--- WellsBankCRM/settings.py
+++ WellsBankCRM/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z0u7))mu3axkng0v!djto#8y!o23ev)7e$frmw@ejj2ugc7$ql
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
